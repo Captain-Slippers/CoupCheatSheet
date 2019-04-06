2d16381e83edf187f04a134aad17390aaf6488cc
Captain Slippers <addy106.9@gmail.com>
2019-04-07 01:04:23 +1000

    Rewriting old code
     - Just rewriting old code to alot simpler

--- src/app/create-sheet/create-sheet.component.ts
+++ src/app/create-sheet/create-sheet.component.ts
@@ -13,11 +13,11 @@ export class CreateSheetComponent implements OnInit {
   ngOnInit() {
   }
 
-  finance =               ["Banker", "Spy" , "Speculator", "Capitalist"];
-  force =                 ["Crime Boss", "General", "Finance", "Judge", "Mercenary"];
-  communications =        ["Director", "Newscaster", "Producer", "Reporter"];
-  specialInterest =       ["Communist", "Customers Officer", "Foreign Consular", "Interllectual", 
-                          "Lawyer", "Peace Keeper", "Politician", "Priest", "Protester"];
+  finance =               ['Banker', 'Spy' , 'Speculator', 'Capitalist'];
+  force =                 ['Crime Boss', 'General', 'Guerrilla', 'Judge', 'Mercenary'];
+  communications =        ['Director', 'Newscaster', 'Producer', 'Reporter'];
+  specialInterest =       ['Communist', 'Customs Officer', 'Foreign Consular', 'Intellectual',
+                          'Lawyer', 'Peace Keeper', 'Politician', 'Priest', 'Protester'];
 
   financeButtons =        [false, false, false, false]
   forceButtons =          [false, false, false, false, false]
@@ -26,15 +26,15 @@ export class CreateSheetComponent implements OnInit {
 
   completedArray = true
 
-  //Array goes: Finance, Force, Communications, Special Interest, Special Interest 
-  currentArray = ["N", "N", "N", "N", "N"];
+  //Array goes: Finance, Force, Communications, Special Interest, Special Interest
+  currentArray = ['N', 'N', 'N', 'N', 'N'];
 
   declaringArray(item: string){
     if (this.finance.includes(item)) {
       this.currentArray[0] = item;
       this.financeButtons = [false, false, false, false];
       this.financeButtons[this.finance.indexOf(item)] = true;
-      
+
       this.checkArray();
     }
 
@@ -42,7 +42,7 @@ export class CreateSheetComponent implements OnInit {
       this.currentArray[1] = item;
       this.forceButtons = [false, false, false, false, false];
       this.forceButtons[this.force.indexOf(item)] = true;
-    
+
       this.checkArray();
     }
 
@@ -50,32 +50,32 @@ export class CreateSheetComponent implements OnInit {
       this.currentArray[2] = item;
       this.communicationsButtons = [false, false, false, false, false];
       this.communicationsButtons[this.communications.indexOf(item)] = true;
-    
+
       this.checkArray();
     }
 
     if (this.specialInterest.includes(item)) {
       this.specialInterestButtons = [false, false, false, false, false, false, false, false, false]
       this.specialInterestButtons[this.specialInterest.indexOf(item)] = true;
-      this.specialInterestButtons[this.specialInterest.indexOf(this.currentArray[3])] = true;    
+      this.specialInterestButtons[this.specialInterest.indexOf(this.currentArray[3])] = true;
 
       this.currentArray[4] = this.currentArray[3];
-      this.currentArray[3] = item; 
-      
+      this.currentArray[3] = item;
+
       this.checkArray();
     }
   }
 
   checkArray(){
-    if (this.currentArray.indexOf("N") == -1 ){
+    if (this.currentArray.indexOf('N') == -1 ){
       this.completedArray = false
     }
   }
 
   createSheet(){
-    var tmp = ""
+    var tmp = ''
     this.currentArray.forEach(i => {
-      tmp = tmp + i + ",";      
+      tmp = tmp + i + ',';
     });
     localStorage.removeItem('Code');
     localStorage.setItem('Code', tmp);
--- src/app/view-sheet/view-sheet.component.ts
+++ src/app/view-sheet/view-sheet.component.ts
@@ -6,46 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-sheet.component.scss']
 })
 export class ViewSheetComponent implements OnInit {
-
-  communicationCards =    ["Director", "Newscaster", "Producer", "Reporter", "Writer"];
-  financeCards =          ["Banker", "Capitalst", "Speculator", "Spy"];
-  forceCards =            ["Crime Boss", "General", "Guerrilla", "Judge", "Mercenary"];
-  specialInterestCards =  ["Communist", "Customers Office", "Foreign Consular", 
-                          "Interllectual", "Lawyer", "Peace Keeper", "Politician", "Priest", "Protester"];
-
-  financeFolder =           "../../assets/images/Finance/";
-  forceFolder =             "../../assets/images/Force/";
-  communicationFolder =     "../../assets/images/Communications/";
-  specialInterestFolder =   "../../assets/images/Special Interest/";
-
-  StorageCards = localStorage.getItem('Code');
-  
-
   constructor() { }
 
+  communicationCards =    ['Director', 'Newscaster', 'Producer', 'Reporter', 'Writer'];
+  financeCards =          ['Banker', 'Capitalst', 'Speculator', 'Spy'];
+  forceCards =            ['Crime Boss', 'General', 'Guerrilla', 'Judge', 'Mercenary'];
+  specialInterestCards =  ['Communist', 'Customs Officer', 'Foreign Consular',
+                          'Intellectual', 'Lawyer', 'Peace Keeper', 'Politician', 'Priest', 'Protester'];
+
+  financeFolder =           '../../assets/images/Finance/';
+  forceFolder =             '../../assets/images/Force/';
+  communicationFolder =     '../../assets/images/Communications/';
+  specialInterestFolder =   '../../assets/images/Special Interest/';
+
+  StorageCards = localStorage.getItem('Code');
+  StorageCards2 = this.StorageCards.split(',');
+  code = '512d2';
+
+
+// tslint:disable-next-line: max-line-length
+  cards =   [this.financeFolder + this.StorageCards2[0],
+             this.forceFolder + this.StorageCards2[1],
+             this.communicationFolder + this.StorageCards2[2],
+             this.specialInterestFolder +  this.StorageCards2[3],
+             this.specialInterestFolder + this.StorageCards2[4]
+            ];
+
   ngOnInit() {
+    console.table(this.StorageCards2);
+    console.log(this.StorageCards);
+
   }
-  
-  code = "512d2";
-  cards = [this.financeFolder + "Banker", this.forceFolder + "Mercenary", this.communicationFolder + "Director", this.specialInterestFolder +  "Protester", this.specialInterestFolder + "Customs Officer"];
 
 
   nextCard(){
-    var tmp;
-    var tmp2 = this.cards[4]
-    console.table(this.cards);
+    // var tmp;
+    // var tmp2 = this.cards[4]
+    // console.table(this.cards);
 
 
-    for (let i = 4; i >= 0; i--) {
-      console.log("Log"+i);
-      if (i == 0){
-        this.cards[i] = tmp2;
-      }
-      else{
-        tmp = this.cards[i];
-        this.cards[i] = this.cards[i-1];
-      }
-      console.table(this.cards);
-    }
+    // for (let i = 4; i >= 0; i--) {
+    //   if (i == 0){
+    //     this.cards[i] = tmp2;
+    //   } else{
+    //     tmp = this.cards[i];
+    //     this.cards[i] = this.cards[i-1];
+    //   }
+    //   // console.table(this.cards);
+    // }
   }
 }
