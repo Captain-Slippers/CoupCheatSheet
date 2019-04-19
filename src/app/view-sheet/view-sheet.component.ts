d2aec150c432125ce9b5c177c839eaefdc5dd25c
Captain Slippers <addy106.9@gmail.com>
2019-04-19 22:30:47 +1000

    Major changes card Slider
     - Allowed for the card slider to display a larger amount of cards depending on the screen size
    
    - Implemented the next and back buttons to work on all screen sizes
    
    - Added edge case for ipad sized screens to not over index leading to only 2 cards displaying
    
    - Minor Styling changes

--- src/app/view-sheet/view-sheet.component.ts
+++ src/app/view-sheet/view-sheet.component.ts
@@ -9,7 +9,7 @@ export class ViewSheetComponent implements OnInit  {
   constructor() {
   }
 
-  index = 0
+  index = 2
 
   financeFolder =           '../../assets/images/Finance/';
   forceFolder =             '../../assets/images/Force/';
@@ -33,13 +33,64 @@ export class ViewSheetComponent implements OnInit  {
     // console.log(this.StorageCards);
   }
 
+  updateScreenSize(){
+    // I don't understand what is happening here but it works
+  }
+
+  trunckIndex(value: number){
+    if (value == 4){
+      value = 3
+    }
+    if (value == 0){
+      value = 1
+    }
+
+    return [value - 1, value, value + 1]
+  }
+
+  styleKeeper(value: number){
+    if (document.body.clientWidth < 1100 && document.body.clientWidth > 700){
+      const trunckedIndex = this.trunckIndex(this.index)
+
+      if (trunckedIndex.includes(value)) {
+        return
+      }
+      else {
+        const styles = {'display' : 'none'};
+        return styles
+      }
+    }
+
+    if (document.body.clientWidth < 700) {
+      if (this.index == value) {
+        return
+      }
+      else {
+        const styles = {'display' : 'none'};
+      return styles
+      }
+    }
+  }
+
+
+
   changeCard(direction){
     if (direction == 'f'){
+      if (document.body.clientWidth < 1100 && document.body.clientWidth > 700){
+        if (this.index < 3){
+          this.index += 1
+        }
+      }
       if (this.index < 4){
         this.index += 1
       }
     }
     else if (direction == 'b'){
+      if (document.body.clientWidth < 1100 && document.body.clientWidth > 700){
+        if (this.index > 1){
+          this.index -= 1
+        }
+      }
       if (this.index > 0){
         this.index -= 1
       }
